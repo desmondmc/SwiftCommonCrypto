55a450a4c4cb3c59f4eace1b0c94153282aee8bf
Desmond McNamee <desmondmcnamee@gmail.com>
2017-06-09 15:34:57 +0200

    First commit

---
+++ Carthage/Build/iOS/CommonCrypto.framework/Headers/CommonCrypto.h
@@ -0,0 +1,19 @@
+//
+//  CommonCrypto.h
+//  CommonCrypto
+//
+//  Created by Desmond McNamee on 2017-06-09.
+//  Copyright © 2017 Desmond McNamee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonCrypto.
+FOUNDATION_EXPORT double CommonCryptoVersionNumber;
+
+//! Project version string for CommonCrypto.
+FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCrypto/PublicHeader.h>
+
+
